af4112b6d1c7e017abcbff272001e3f5f03a22d4
Peter C <myemail@email.com>
2025-01-25 13:44:18 +1000

    Add CDK project for myExistingS3 bucket

--- my_existing_s3_bucket/my_existing_s3_bucket_stack.py
+++ my_existing_s3_bucket/my_existing_s3_bucket_stack.py
@@ -1,19 +1,67 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import Stack
+import aws_cdk as cdk
+import aws_cdk.aws_s3 as s3
 from constructs import Construct
 
 class MyExistingS3BucketStack(Stack):
+  def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    # Resources
+    s3Bucket00aaclsands32024011900pIrkZ = s3.CfnBucket(self, 'S3Bucket00aaclsands32024011900pIrkZ',
+          public_access_block_configuration = {
+            'restrictPublicBuckets': True,
+            'ignorePublicAcls': True,
+            'blockPublicPolicy': True,
+            'blockPublicAcls': True,
+          },
+          bucket_name = 'aaclsands320240119',
+          ownership_controls = {
+            'rules': [
+              {
+                'objectOwnership': 'BucketOwnerEnforced',
+              },
+            ],
+          },
+          bucket_encryption = {
+            'serverSideEncryptionConfiguration': [
+              {
+                'bucketKeyEnabled': True,
+                'serverSideEncryptionByDefault': {
+                  'sseAlgorithm': 'AES256',
+                },
+              },
+            ],
+          },
+        )
+    s3Bucket00aaclsands32024011900pIrkZ.cfn_options.deletion_policy = cdk.CfnDeletionPolicy.RETAIN
+    s3Bucket00aaclsands32024011900pIrkZ.cfn_options.update_replace_policy = cdk.CfnDeletionPolicy.RETAIN
 
-        # The code that defines your stack goes here
+    s3BucketPolicy00aaclsands32024011900Gp3Ut = s3.CfnBucketPolicy(self, 'S3BucketPolicy00aaclsands32024011900Gp3Ut',
+          bucket = s3Bucket00aaclsands32024011900pIrkZ.ref,
+          policy_document = {
+            'Version': '2012-10-17',
+            'Statement': [
+              {
+                'Condition': {
+                  'Bool': {
+                    'aws:SecureTransport': 'false',
+                  },
+                },
+                'Resource': [
+                  'arn:aws:s3:::aaclsands320240119',
+                  'arn:aws:s3:::aaclsands320240119/*',
+                ],
+                'Action': 's3:*',
+                'Effect': 'Deny',
+                'Principal': '*',
+                'Sid': 'RestrictToTLSRequestsOnly',
+              },
+            ],
+            'Id': 'PutObjPolicy',
+          },
+        )
+    s3BucketPolicy00aaclsands32024011900Gp3Ut.cfn_options.deletion_policy = cdk.CfnDeletionPolicy.RETAIN
+    s3BucketPolicy00aaclsands32024011900Gp3Ut.cfn_options.update_replace_policy = cdk.CfnDeletionPolicy.RETAIN
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MyExistingS3BucketQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+
